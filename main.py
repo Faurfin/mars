c4921f2cde9ded5cf8de90a7bb32c020ac789270
Faurfin <neadekvat448@gmail.com>
2026-05-01 14:17:17 +0300

    Список профессий

--- main.py
+++ main.py
@@ -373,10 +373,38 @@ def carousel():
 </html>'''
 
 
-    @app.route('/training/<prof>')
+@app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof.lower(), title='Тренировки')
 
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions = [
+        "инженер-исследователь",
+        "пилот",
+        "строитель",
+        "экзобиолог",
+        "врач",
+        "инженер по терраформированию",
+        "климатолог",
+        "специалист по радиационной защите",
+        "астрогеолог",
+        "гляциолог",
+        "инженер жизнеобеспечения",
+        "метеоролог",
+        "оператор марсохода",
+        "киберинженер",
+        "штурман",
+        "пилот дронов"
+    ]
+
+    return render_template(
+        'list_prof.html',
+        list_type=list_type,
+        professions=professions,
+        title='Список профессий'
+    )
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
